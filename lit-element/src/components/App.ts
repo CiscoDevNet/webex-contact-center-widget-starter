02691d690e58afa1aa02242254696b29ae432f41
Kevin Hyde <kehyde@cisco.com>
2020-12-08 12:49:16 -0800

    add attributes with updates and map styles

--- lit-element/src/components/App.ts
+++ lit-element/src/components/App.ts
@@ -27,20 +27,225 @@ const loader = new Loader({
   version: "weekly"
 });
 
+const stylesArray: google.maps.MapTypeStyle[] = [
+  {
+    featureType: "all",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "off"
+      },
+      {
+        color: "#f49f53"
+      }
+    ]
+  },
+  {
+    featureType: "all",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "simplified"
+      }
+    ]
+  },
+  {
+    featureType: "landscape",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#f9ddc5"
+      },
+      {
+        lightness: -7
+      }
+    ]
+  },
+  {
+    featureType: "poi.business",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#645c20"
+      },
+      {
+        lightness: 38
+      }
+    ]
+  },
+  {
+    featureType: "poi.government",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#9e5916"
+      },
+      {
+        lightness: 46
+      }
+    ]
+  },
+  {
+    featureType: "poi.medical",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#813033"
+      },
+      {
+        lightness: 38
+      },
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "poi.park",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#645c20"
+      },
+      {
+        lightness: 39
+      }
+    ]
+  },
+  {
+    featureType: "poi.school",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#a95521"
+      },
+      {
+        lightness: 35
+      }
+    ]
+  },
+  {
+    featureType: "poi.sports_complex",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#9e5916"
+      },
+      {
+        lightness: 32
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#813033"
+      },
+      {
+        lightness: 43
+      }
+    ]
+  },
+  {
+    featureType: "road.local",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#f19f53"
+      },
+      {
+        weight: 1.3
+      },
+      {
+        visibility: "on"
+      },
+      {
+        lightness: 16
+      }
+    ]
+  },
+  {
+    featureType: "road.local",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#f19f53"
+      },
+      {
+        lightness: -10
+      }
+    ]
+  },
+  {
+    featureType: "transit",
+    elementType: "all",
+    stylers: [
+      {
+        lightness: 38
+      }
+    ]
+  },
+  {
+    featureType: "transit.line",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#813033"
+      },
+      {
+        lightness: 22
+      }
+    ]
+  },
+  {
+    featureType: "transit.station",
+    elementType: "all",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "all",
+    stylers: [
+      {
+        color: "#1994bf"
+      },
+      {
+        saturation: -69
+      },
+      {
+        gamma: 0.99
+      },
+      {
+        lightness: 43
+      }
+    ]
+  }
+];
+
 @customElement("my-custom-component")
 export default class MyCustomComponent extends LitElement {
-  @property({ type: Number }) latitude = 47.6062;
-  @property({ type: Number }) longitude = -122.3321;
-  @internalProperty()
-  map?: google.maps.Map;
+  @property({ type: Number, reflect: true }) latitude = 47.6062;
+  @property({ type: Number, reflect: true }) longitude = -122.3321;
+  @property({ type: String, reflect: true }) address =
+    "3211 Belvidere Ave SW, Seattle WA 98126";
+  @property({ type: Number, reflect: true }) zoom = 8;
+
+  @internalProperty() map?: google.maps.Map;
   @query("#map") mapDiv?: HTMLElement;
 
   initMap = (): void => {
     loader.load().then(() => {
       if (this.mapDiv) {
         this.map = new google.maps.Map(this.mapDiv, {
-          center: { lat: 47.6062, lng: -122.3321 },
-          zoom: 8
+          center: { lat: this.latitude, lng: this.longitude },
+          zoom: this.zoom,
+          styles: stylesArray
         });
       }
       console.log(this.mapDiv);
@@ -52,6 +257,17 @@ export default class MyCustomComponent extends LitElement {
     console.log(this.mapDiv);
     super.firstUpdated(changedProperties);
     this.initMap();
+  }
+  update(changedProperties: PropertyValues) {
+    super.update(changedProperties);
+    console.log(changedProperties);
+    if (
+      changedProperties.has("latitude") ||
+      changedProperties.has("zoom") ||
+      changedProperties.has("longitude")
+    ) {
+      this.initMap();
+    }
   }
 
   static get styles() {
