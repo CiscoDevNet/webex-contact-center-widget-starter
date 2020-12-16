58699059c79abe3182c2aaec20b4144c752b8f85
Kevin Hyde <kehyde@cisco.com>
2020-12-15 16:12:43 -0800

    Add places API, change and modularize mapstyles

--- lit-element/src/components/App.ts
+++ lit-element/src/components/App.ts
@@ -18,234 +18,32 @@ import {
 import styles from "./App.scss";
 import { Loader } from "@googlemaps/js-api-loader";
 // import {} from "googlemaps";
-
-// let map: google.maps.Map;
-// const center: google.maps.LatLngLiteral = { lat: 30, lng: -110 };
-
-const loader = new Loader({
-  apiKey: "AIzaSyDlVE8flzwPFbSzuGdaaatrGrG3WIULw5I",
-  version: "weekly"
-});
-
-const stylesArray: google.maps.MapTypeStyle[] = [
-  {
-    featureType: "all",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off"
-      },
-      {
-        color: "#f49f53"
-      }
-    ]
-  },
-  {
-    featureType: "all",
-    elementType: "labels.text",
-    stylers: [
-      {
-        visibility: "simplified"
-      }
-    ]
-  },
-  {
-    featureType: "landscape",
-    elementType: "all",
-    stylers: [
-      {
-        color: "#f9ddc5"
-      },
-      {
-        lightness: -7
-      }
-    ]
-  },
-  {
-    featureType: "poi.business",
-    elementType: "all",
-    stylers: [
-      {
-        color: "#645c20"
-      },
-      {
-        lightness: 38
-      }
-    ]
-  },
-  {
-    featureType: "poi.government",
-    elementType: "all",
-    stylers: [
-      {
-        color: "#9e5916"
-      },
-      {
-        lightness: 46
-      }
-    ]
-  },
-  {
-    featureType: "poi.medical",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        color: "#813033"
-      },
-      {
-        lightness: 38
-      },
-      {
-        visibility: "off"
-      }
-    ]
-  },
-  {
-    featureType: "poi.park",
-    elementType: "all",
-    stylers: [
-      {
-        color: "#645c20"
-      },
-      {
-        lightness: 39
-      }
-    ]
-  },
-  {
-    featureType: "poi.school",
-    elementType: "all",
-    stylers: [
-      {
-        color: "#a95521"
-      },
-      {
-        lightness: 35
-      }
-    ]
-  },
-  {
-    featureType: "poi.sports_complex",
-    elementType: "all",
-    stylers: [
-      {
-        color: "#9e5916"
-      },
-      {
-        lightness: 32
-      }
-    ]
-  },
-  {
-    featureType: "road",
-    elementType: "all",
-    stylers: [
-      {
-        color: "#813033"
-      },
-      {
-        lightness: 43
-      }
-    ]
-  },
-  {
-    featureType: "road.local",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        color: "#f19f53"
-      },
-      {
-        weight: 1.3
-      },
-      {
-        visibility: "on"
-      },
-      {
-        lightness: 16
-      }
-    ]
-  },
-  {
-    featureType: "road.local",
-    elementType: "geometry.stroke",
-    stylers: [
-      {
-        color: "#f19f53"
-      },
-      {
-        lightness: -10
-      }
-    ]
-  },
-  {
-    featureType: "transit",
-    elementType: "all",
-    stylers: [
-      {
-        lightness: 38
-      }
-    ]
-  },
-  {
-    featureType: "transit.line",
-    elementType: "all",
-    stylers: [
-      {
-        color: "#813033"
-      },
-      {
-        lightness: 22
-      }
-    ]
-  },
-  {
-    featureType: "transit.station",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "off"
-      }
-    ]
-  },
-  {
-    featureType: "water",
-    elementType: "all",
-    stylers: [
-      {
-        color: "#1994bf"
-      },
-      {
-        saturation: -69
-      },
-      {
-        gamma: 0.99
-      },
-      {
-        lightness: 43
-      }
-    ]
-  }
-];
-
+import { map } from "./lightMap";
 @customElement("my-custom-component")
 export default class MyCustomComponent extends LitElement {
   @property({ type: Number, reflect: true }) latitude = 47.6062;
   @property({ type: Number, reflect: true }) longitude = -122.3321;
   @property({ type: String, reflect: true }) address =
     "3211 Belvidere Ave SW, Seattle WA 98126";
+  @property({ type: String, reflect: true, attribute: "api-key" }) apiKey = "";
   @property({ type: Number, reflect: true }) zoom = 8;
 
   @internalProperty() map?: google.maps.Map;
   @query("#map") mapDiv?: HTMLElement;
 
   initMap = (): void => {
+    const loader = new Loader({
+      apiKey: this.apiKey,
+      version: "weekly",
+      libraries: ["places"]
+    });
+
     loader.load().then(() => {
       if (this.mapDiv) {
         this.map = new google.maps.Map(this.mapDiv, {
           center: { lat: this.latitude, lng: this.longitude },
           zoom: this.zoom,
-          styles: stylesArray
+          styles: map
         });
       }
       console.log(this.mapDiv);
@@ -257,14 +55,35 @@ export default class MyCustomComponent extends LitElement {
     console.log(this.mapDiv);
     super.firstUpdated(changedProperties);
     this.initMap();
+    setTimeout(() => {
+      this.getAddressMap();
+    }, 800);
   }
+
+  getAddressMap = () => {
+    if (this.map) {
+      const places = new google.maps.places.PlacesService(this.map);
+      const query = this.address.split(" ").join("%");
+      console.log(query);
+
+      places.textSearch(
+        {
+          query: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${query}&inputtype=textquery&key=${this.apiKey}`
+        },
+        data => {
+          console.log(data);
+        }
+      );
+    }
+  };
+
   update(changedProperties: PropertyValues) {
     super.update(changedProperties);
-    console.log(changedProperties);
     if (
+      changedProperties.has("address") ||
       changedProperties.has("latitude") ||
-      changedProperties.has("zoom") ||
-      changedProperties.has("longitude")
+      changedProperties.has("longitude") ||
+      changedProperties.has("zoom")
     ) {
       this.initMap();
     }
@@ -276,9 +95,7 @@ export default class MyCustomComponent extends LitElement {
   render() {
     return html`
       <div class="container">
-        <h1>Map Widget</h1>
         <div id="map"></div>
-        <slot></slot>
       </div>
     `;
   }
--- lit-element/src/index.ts
+++ lit-element/src/index.ts
@@ -6,7 +6,13 @@
  *
  */
 
-import { html, LitElement, customElement, css, internalProperty } from "lit-element";
+import {
+  html,
+  LitElement,
+  customElement,
+  css,
+  internalProperty
+} from "lit-element";
 import { agentxJsApi } from "@agentx/agentx-js-api";
 import { Service } from "@agentx/agentx-services";
 import "./components/App";
@@ -16,8 +22,10 @@ import "./components/App";
 @customElement("my-custom-widget")
 export default class MyCustomComponent extends LitElement {
   @internalProperty() private contacts: string[] = [];
-  
-  private readonly logger = agentxJsApi.logger.createLogger("[Widget-Using-JSAPI]");
+
+  private readonly logger = agentxJsApi.logger.createLogger(
+    "[Widget-Using-JSAPI]"
+  );
 
   static get styles() {
     return css`
@@ -52,33 +60,45 @@ export default class MyCustomComponent extends LitElement {
 
   // Subscribe AgentCONTACT
   subscribeAgentContactDataEvents() {
-    agentxJsApi.agentContact.addEventListener("eAgentContact", (msg: Service.Aqm.Contact.AgentContact) =>
-      console.log("AgentContact eAgentContact: ", msg.data)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentContact",
+      (msg: Service.Aqm.Contact.AgentContact) =>
+        console.log("AgentContact eAgentContact: ", msg.data)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentContactAssigned", (msg: any) =>
-      console.log("AgentContact eAgentContactAssigned: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentContactAssigned",
+      (msg: any) => console.log("AgentContact eAgentContactAssigned: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentContactEnded", (msg: any) =>
-      console.log("AgentContact eAgentContactEnded: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentContactEnded",
+      (msg: any) => console.log("AgentContact eAgentContactEnded: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentContactWrappedUp", (msg: any) =>
-      console.log("AgentContact eAgentContactWrappedUp: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentContactWrappedUp",
+      (msg: any) => console.log("AgentContact eAgentContactWrappedUp: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentOfferContact", (msg: { data: { interactionId: string; }; }) => {
-      console.log("AgentContact eAgentOfferContact: ", msg);
-      // AUX Sandbox Contact
-      this.contacts.push(msg.data.interactionId);
-    });
-    agentxJsApi.agentContact.addEventListener("eAgentOfferContactRona", (msg: { data: { interactionId: string; }; }) => {
-      console.log("AgentContact eAgentOfferContactRona: ", msg);
-      // AUX Sandbox Contact
-      const idx = this.contacts.indexOf(msg.data.interactionId);
-      if (idx != -1) {
-        this.contacts.slice(idx, 1);
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentOfferContact",
+      (msg: { data: { interactionId: string } }) => {
+        console.log("AgentContact eAgentOfferContact: ", msg);
+        // AUX Sandbox Contact
+        this.contacts.push(msg.data.interactionId);
       }
-    });
-    agentxJsApi.agentContact.addEventListener("eAgentOfferConsult", (msg: any) =>
-      console.log("AgentContact eAgentOfferConsult: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentOfferContactRona",
+      (msg: { data: { interactionId: string } }) => {
+        console.log("AgentContact eAgentOfferContactRona: ", msg);
+        // AUX Sandbox Contact
+        const idx = this.contacts.indexOf(msg.data.interactionId);
+        if (idx != -1) {
+          this.contacts.slice(idx, 1);
+        }
+      }
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentOfferConsult",
+      (msg: any) => console.log("AgentContact eAgentOfferConsult: ", msg)
     );
     agentxJsApi.agentContact.addEventListener("eAgentWrapup", (msg: any) =>
       console.log("AgentContact eAgentWrapup: ", msg)
@@ -86,87 +106,143 @@ export default class MyCustomComponent extends LitElement {
     agentxJsApi.agentContact.addEventListener("eAgentContactHeld", (msg: any) =>
       console.log("AgentContact eAgentContactHeld: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentContactUnHeld", (msg: any) =>
-      console.log("AgentContact eAgentContactUnHeld: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentContactUnHeld",
+      (msg: any) => console.log("AgentContact eAgentContactUnHeld: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eCallRecordingStarted", (msg: any) =>
-      console.log("AgentContact eCallRecordingStarted: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eCallRecordingStarted",
+      (msg: any) => console.log("AgentContact eCallRecordingStarted: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentConsultCreated", (msg: any) =>
-      console.log("AgentContact eAgentConsultCreated: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentConsultCreated",
+      (msg: any) => console.log("AgentContact eAgentConsultCreated: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentConsultConferenced", (msg: any) =>
-      console.log("AgentContact eAgentConsultConferenced: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentConsultConferenced",
+      (msg: any) => console.log("AgentContact eAgentConsultConferenced: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentConsultEnded", (msg: any) =>
-      console.log("AgentContact eAgentConsultEnded: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentConsultEnded",
+      (msg: any) => console.log("AgentContact eAgentConsultEnded: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentCtqCancelled", (msg: any) =>
-      console.log("AgentContact eAgentCtqCancelled: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentCtqCancelled",
+      (msg: any) => console.log("AgentContact eAgentCtqCancelled: ", msg)
     );
     agentxJsApi.agentContact.addEventListener("eAgentConsulting", (msg: any) =>
       console.log("AgentContact eAgentConsulting: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentConsultFailed", (msg: any) =>
-      console.log("AgentContact eAgentConsultFailed: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentConsultFailed",
+      (msg: any) => console.log("AgentContact eAgentConsultFailed: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentConsultEndFailed", (msg: any) =>
-      console.log("AgentContact eAgentConsultEndFailed: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentConsultEndFailed",
+      (msg: any) => console.log("AgentContact eAgentConsultEndFailed: ", msg)
     );
     agentxJsApi.agentContact.addEventListener("eAgentCtqFailed", (msg: any) =>
       console.log("AgentContact eAgentCtqFailed: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentCtqCancelFailed", (msg: any) =>
-      console.log("AgentContact eAgentCtqCancelFailed: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentCtqCancelFailed",
+      (msg: any) => console.log("AgentContact eAgentCtqCancelFailed: ", msg)
     );
-    agentxJsApi.agentContact.addEventListener("eAgentConsultConferenceEndFailed", (msg: any) =>
-      console.log("AgentContact eAgentConsultConferenceEndFailed: ", msg)
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentConsultConferenceEndFailed",
+      (msg: any) =>
+        console.log("AgentContact eAgentConsultConferenceEndFailed: ", msg)
     );
   }
 
   // Subscribe DIALER
   subscribeDialerDataEvents() {
-    agentxJsApi.dialer.addEventListener("eOutdialFailed", (msg: any) => console.log("Dialer eOutdialFailed: ", msg));
+    agentxJsApi.dialer.addEventListener("eOutdialFailed", (msg: any) =>
+      console.log("Dialer eOutdialFailed: ", msg)
+    );
   }
 
   ////// ScreenPop JSAPI
   // Subscribe SCREENPOP
   subscribeScreenPopDataEvents() {
-    agentxJsApi.screenpop.addEventListener("eScreenPop", (msg: any) => console.log("ScreenPop eScreenPop: ", msg));
+    agentxJsApi.screenpop.addEventListener("eScreenPop", (msg: any) =>
+      console.log("ScreenPop eScreenPop: ", msg)
+    );
   }
 
   // Subscribe NOTIFICATIONS
   subscribeNotificationsEvents() {
     agentxJsApi.notifications.addEventListener("add", n => {
       console.log("Notifications Added: ", n);
-      console.log("Notifications Total: ", agentxJsApi.notifications.added.length);
+      console.log(
+        "Notifications Total: ",
+        agentxJsApi.notifications.added.length
+      );
     });
     agentxJsApi.notifications.addEventListener("pending", n => {
       console.log("Notifications Pending: ", n);
-      console.log("Notifications Total Added: ", agentxJsApi.notifications.added.length);
-      console.log("Notifications Total Pending: ", agentxJsApi.notifications.pended.length);
-      console.log("Notifications Total Activated: ", agentxJsApi.notifications.activated.length);
-      console.log("Notifications Total Deactivated: ", agentxJsApi.notifications.deactivated.length);
+      console.log(
+        "Notifications Total Added: ",
+        agentxJsApi.notifications.added.length
+      );
+      console.log(
+        "Notifications Total Pending: ",
+        agentxJsApi.notifications.pended.length
+      );
+      console.log(
+        "Notifications Total Activated: ",
+        agentxJsApi.notifications.activated.length
+      );
+      console.log(
+        "Notifications Total Deactivated: ",
+        agentxJsApi.notifications.deactivated.length
+      );
     });
     agentxJsApi.notifications.addEventListener("activate", n => {
       console.log("Notifications Activated: ", n);
-      console.log("Notifications Total Added: ", agentxJsApi.notifications.added.length);
-      console.log("Notifications Total Pending: ", agentxJsApi.notifications.pended.length);
-      console.log("Notifications Total Activated: ", agentxJsApi.notifications.activated.length);
-      console.log("Notifications Total Deactivated: ", agentxJsApi.notifications.deactivated.length);
+      console.log(
+        "Notifications Total Added: ",
+        agentxJsApi.notifications.added.length
+      );
+      console.log(
+        "Notifications Total Pending: ",
+        agentxJsApi.notifications.pended.length
+      );
+      console.log(
+        "Notifications Total Activated: ",
+        agentxJsApi.notifications.activated.length
+      );
+      console.log(
+        "Notifications Total Deactivated: ",
+        agentxJsApi.notifications.deactivated.length
+      );
     });
     agentxJsApi.notifications.addEventListener("deactivate", n => {
       console.log("Notifications Deactivated: ", n);
-      console.log("Notifications Total Added: ", agentxJsApi.notifications.added.length);
-      console.log("Notifications Total Pending: ", agentxJsApi.notifications.pended.length);
-      console.log("Notifications Total Activated: ", agentxJsApi.notifications.activated.length);
-      console.log("Notifications Total Deactivated: ", agentxJsApi.notifications.deactivated.length);
+      console.log(
+        "Notifications Total Added: ",
+        agentxJsApi.notifications.added.length
+      );
+      console.log(
+        "Notifications Total Pending: ",
+        agentxJsApi.notifications.pended.length
+      );
+      console.log(
+        "Notifications Total Activated: ",
+        agentxJsApi.notifications.activated.length
+      );
+      console.log(
+        "Notifications Total Deactivated: ",
+        agentxJsApi.notifications.deactivated.length
+      );
     });
   }
-  
+
   render() {
     return html`
-      <my-custom-component></my-custom-component>
+      <my-custom-component
+        api-key="AIzaSyDlVE8flzwPFbSzuGdaaatrGrG3WIULw5I"
+      ></my-custom-component>
     `;
   }
 }
