f470456e3c3c163132a1f9e89dd7d5c9a74cba89
Kevin Hyde <kehyde@cisco.com>
2020-12-18 12:06:25 -0800

    refactor pin updates, add darktheme toggle

--- lit-element/src/components/App.ts
+++ lit-element/src/components/App.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
 /**
  * Copyright (c) Cisco Systems, Inc. and its affiliates.
  *
@@ -18,7 +19,8 @@ import {
 import styles from "./App.scss";
 import { Loader } from "@googlemaps/js-api-loader";
 // import {} from "googlemaps";
-import { map } from "./lightMap";
+import { map as lightMap } from "./lightMap";
+import { darkMap } from "./darkMap";
 @customElement("my-custom-component")
 export default class MyCustomComponent extends LitElement {
   @property({ type: Number, reflect: true }) latitude = 47.6062;
@@ -27,10 +29,45 @@ export default class MyCustomComponent extends LitElement {
   @property({ type: String, reflect: true, attribute: "api-key" }) apiKey = "";
   @property({ type: Number, reflect: true }) zoom = 12;
 
+  @internalProperty() lumos = false;
+  @internalProperty() darkTheme = false;
+
   @internalProperty() map?: google.maps.Map;
   @internalProperty() markers?: google.maps.places.PlaceResult[];
   @query("#map") mapDiv?: HTMLElement;
   @query("#pac-input") searchInput?: HTMLInputElement;
+
+  @internalProperty() loader = new Loader({
+    apiKey: this.apiKey,
+    version: "weekly",
+    libraries: ["places"]
+  });
+
+  closestElement(selector: string, base = this) {
+    function __closestFrom(el: unknown): HTMLElement | null {
+      if (!el || el === document || el === window) return null;
+      // @ts-ignore
+      const found = el.closest(selector);
+      // @ts-ignore
+      return found ? found : __closestFrom(el.getRootNode().host);
+    }
+    return __closestFrom(base);
+  }
+
+  refreshTokenData = () => {
+    const themeWrapper: unknown = this.closestElement("md-theme");
+    // @ts-ignore
+    this.lumos = themeWrapper.lumos;
+    // @ts-ignore
+    this.darkTheme = themeWrapper.darkTheme;
+    const mapTheme = this.darkTheme ? darkMap : lightMap;
+    this.map?.setOptions({ styles: mapTheme });
+  };
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.loader.apiKey = this.apiKey;
+  }
 
   firstUpdated(changedProperties: PropertyValues) {
     super.firstUpdated(changedProperties);
@@ -49,37 +86,79 @@ export default class MyCustomComponent extends LitElement {
     }
   }
 
-  initMap = () => {
-    const loader = new Loader({
-      apiKey: this.apiKey,
-      version: "weekly",
-      libraries: ["places"]
-    });
+  updatePins = (places: google.maps.places.PlaceResult[]) => {
+    console.log("Update Pins Function");
+    let markers: google.maps.Marker[] = [];
 
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(marker => {
+      console.log(marker);
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+    places.forEach(place => {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      const icon = {
+        url: place.icon as string,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map: this.map,
+          icon,
+          title: place.name,
+          position: place.geometry.location
+        })
+      );
+
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    this.map!.fitBounds(bounds);
+  };
+
+  initMap = () => {
     if (!this.map) {
-      loader
+      console.log("NO MAP DETECTED");
+      const mapTheme = this.darkTheme ? darkMap : lightMap;
+      this.loader
         .load()
-        .then(() => this.getAddressMap())
+        .then(() => this.updateAttributeSearch())
         .then(() => {
-          console.log("FART TWO");
           if (this.mapDiv) {
             this.map = new google.maps.Map(this.mapDiv, {
               center: { lat: this.latitude, lng: this.longitude },
               zoom: this.zoom,
-              styles: map,
+              styles: mapTheme,
               mapTypeControl: false
             });
 
             this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
               this.searchInput as Node
             );
+
             // Create the search box and link it to the UI element.
             const searchBox = new google.maps.places.SearchBox(
               this.searchInput!
             );
-            // this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
-            //   this.searchInput!
-            // );
 
             // Bias the SearchBox results towards current map's viewport.
             this.map.addListener("bounds_changed", () => {
@@ -88,99 +167,37 @@ export default class MyCustomComponent extends LitElement {
               );
             });
 
-            let markers: google.maps.Marker[] = [];
             // Listen for the event fired when the user selects a prediction and retrieve
             // more details for that place.
             searchBox.addListener("places_changed", () => {
-              const places = searchBox.getPlaces();
-
-              if (places.length == 0) {
-                return;
-              }
-
-              // Clear out the old markers.
-              markers.forEach(marker => {
-                marker.setMap(null);
-              });
-              markers = [];
-
-              // For each place, get the icon, name and location.
-              const bounds = new google.maps.LatLngBounds();
-              places.forEach(place => {
-                if (!place.geometry) {
-                  console.log("Returned place contains no geometry");
-                  return;
-                }
-                const icon = {
-                  url: place.icon as string,
-                  size: new google.maps.Size(71, 71),
-                  origin: new google.maps.Point(0, 0),
-                  anchor: new google.maps.Point(17, 34),
-                  scaledSize: new google.maps.Size(25, 25)
-                };
-
-                // Create a marker for each place.
-                markers.push(
-                  new google.maps.Marker({
-                    map: this.map,
-                    icon,
-                    title: place.name,
-                    position: place.geometry.location
-                  })
-                );
-
-                if (place.geometry.viewport) {
-                  // Only geocodes have viewport.
-                  bounds.union(place.geometry.viewport);
-                } else {
-                  bounds.extend(place.geometry.location);
-                }
-              });
-              this.map!.fitBounds(bounds);
-            });
-
-            new google.maps.Marker({
-              position: { lat: this.latitude, lng: this.longitude },
-              map: this.map,
-              title: "Hello World!"
+              console.log("Google API event Listener");
+              this.updatePins(searchBox.getPlaces());
             });
           }
         });
     } else {
-      this.getAddressMap()
-        .then(() =>
-          this.map?.panTo({ lat: this.latitude, lng: this.longitude })
-        )
-        .then(() => {
-          new google.maps.Marker({
-            position: { lat: this.latitude, lng: this.longitude },
-            map: this.map,
-            title: "Hello World!"
-          });
-        });
+      this.updateAttributeSearch().then(() => {
+        const mapTheme = this.darkTheme ? darkMap : lightMap;
+        console.log(mapTheme);
+        this.map?.panTo({ lat: this.latitude, lng: this.longitude });
+        this.map?.setOptions({ styles: darkMap });
+      });
     }
   };
 
-  getAddressMap = async () => {
-    console.log("FART ONE");
+  updateAttributeSearch = async () => {
     if (this.map) {
       const places = new google.maps.places.PlacesService(this.map);
-      const query = this.search.split(" ").join("%");
+      const attributeQuery = this.search.split(" ").join("%");
 
       places.nearbySearch(
         {
-          keyword: query,
+          keyword: attributeQuery,
           location: { lat: this.latitude, lng: this.longitude },
           radius: 50000
         },
-        (results, status) => {
-          console.log(query, results, status);
-          // if (data.length > 1) {
-          //   debugger;
-          // }
-          // this.markers = data;
-          // this.latitude = data[0].geometry?.location.lat()!;
-          // this.longitude = data[0].geometry?.location.lng()!;
+        results => {
+          this.updatePins(results);
         }
       );
     }
@@ -193,13 +210,14 @@ export default class MyCustomComponent extends LitElement {
   render() {
     return html`
       <div class="container">
-        <input
+        <md-input
           id="pac-input"
           class="controls"
           type="text"
           placeholder="Search Box"
-        />
+        ></md-input>
         <div id="map"></div>
+        <md-button @click=${this.refreshTokenData}>Update theme</md-button>
       </div>
     `;
   }
