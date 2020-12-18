710b8287154019c55aa2d5473f96e83bfeca6408
Yana Harris <yanaha@cisco.com>
2020-12-18 14:05:50 -0800

    adding more listeners

--- lit-element/src/components/App.ts
+++ lit-element/src/components/App.ts
@@ -10,18 +10,19 @@ import { agentxJsApi } from "@agentx/agentx-js-api";
 import { html, LitElement, customElement, internalProperty } from "lit-element";
 import styles from "./App.scss";
 import { logger } from "./sdk";
-
+import { Service } from "@agentx/agentx-services-types";
 @customElement("my-custom-component")
 export default class MyCustomComponent extends LitElement {
   /**
    * Replace this with the logic to obtain interaction ID you need
    * through agentxJsApi.actions sub-module or through external props
    */
-  @internalProperty() sampleInteractionId =
+  @internalProperty() sampleInteractionId: Service.Aqm.Contact.Interaction["interactionId"] =
     "58f76ca3-409f-11eb-8606-f1b296a9b969";
   @internalProperty() agentId = "7d12d9ea-e8e0-41ee-81bf-c11a685b64ed";
-  @internalProperty() buddyAgents: any = null;
-  @internalProperty() vTeam: any = null;
+  @internalProperty() buddyAgents: Service.Aqm.Contact.BuddyAgentsSuccess | null = null;
+  @internalProperty() vTeam: Service.Aqm.Contact.VTeamSuccess | null = null;
+  @internalProperty() contacts: Service.Aqm.Contact.Interaction["interactionId"][] = [];
 
   static get styles() {
     return styles;
@@ -30,15 +31,127 @@ export default class MyCustomComponent extends LitElement {
     super.connectedCallback();
     this.init();
     this.getCurrentInteractionId();
+    this.subscribeAgentContactDataEvents()
+    this.subscribeDialerEvents();
+    this.subscribeScreenpopEvent();
+  }
+
+  disconnectedCallback() {
+    super. disconnectedCallback();
+    agentxJsApi.agentContact.removeAllEventListeners();
+    agentxJsApi.dialer.removeAllEventListeners();
+    agentxJsApi.screenpop.removeAllEventListeners()
   }
 
   async init() {
     await agentxJsApi.config.init();
   }
 
+  subscribeScreenpopEvent() {
+    agentxJsApi.screenpop.addEventListener("eScreenPop", (msg: any) => logger.info(msg));
+  }
+
+  subscribeDialerEvents() {
+    agentxJsApi.dialer.addEventListener("eOutdialFailed", (msg: any) => logger.info(msg));
+  }
+
+  subscribeAgentContactDataEvents() {
+    agentxJsApi.agentContact.addEventListener("eAgentContact", (msg: any) =>
+      logger.info("AgentContact eAgentContact: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentContactAssigned",
+      (msg: any) => logger.info("AgentContact eAgentContactAssigned: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentContactEnded",
+      (msg: any) => logger.info("AgentContact eAgentContactEnded: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentContactWrappedUp",
+      (msg: any) => logger.info("AgentContact eAgentContactWrappedUp: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentOfferContact",
+      (msg: Service.Aqm.Contact.AgentContact) => {
+        logger.info("AgentContact eAgentOfferContact: ", msg);
+        // AUX Sandbox Contact
+        this.contacts.push(msg.data.interactionId);
+      }
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentOfferContactRona",
+      (msg: Service.Aqm.Contact.AgentContact) => {
+        logger.info("AgentContact eAgentOfferContactRona: ", msg);
+        // AUX Sandbox Contact
+        const idx = this.contacts.indexOf(msg.data.interactionId);
+        if (idx != -1) {
+          this.contacts.slice(idx, 1);
+        }
+      }
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentOfferConsult",
+      (msg: any) => logger.info("AgentContact eAgentOfferConsult: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener("eAgentWrapup", (msg: any) =>
+      logger.info("AgentContact eAgentWrapup: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener("eAgentContactHeld", (msg: any) =>
+      logger.info("AgentContact eAgentContactHeld: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentContactUnHeld",
+      (msg: any) => logger.info("AgentContact eAgentContactUnHeld: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eCallRecordingStarted",
+      (msg: any) => logger.info("AgentContact eCallRecordingStarted: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentConsultCreated",
+      (msg: any) => logger.info("AgentContact eAgentConsultCreated: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentConsultConferenced",
+      (msg: any) => logger.info("AgentContact eAgentConsultConferenced: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentConsultEnded",
+      (msg: any) => logger.info("AgentContact eAgentConsultEnded: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentCtqCancelled",
+      (msg: any) => logger.info("AgentContact eAgentCtqCancelled: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener("eAgentConsulting", (msg: any) =>
+      logger.info("AgentContact eAgentConsulting: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentConsultFailed",
+      (msg: any) => logger.info("AgentContact eAgentConsultFailed: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentConsultEndFailed",
+      (msg: any) => logger.info("AgentContact eAgentConsultEndFailed: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener("eAgentCtqFailed", (msg: any) =>
+      logger.info("AgentContact eAgentCtqFailed: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentCtqCancelFailed",
+      (msg: any) => logger.info("AgentContact eAgentCtqCancelFailed: ", msg)
+    );
+    agentxJsApi.agentContact.addEventListener(
+      "eAgentConsultConferenceEndFailed",
+      (msg: any) =>
+        logger.info("AgentContact eAgentConsultConferenceEndFailed: ", msg)
+    );
+  }
+
   getCurrentInteractionId() {
     let path = window.location.pathname;
-    if (path.indexOf("task") >=0 && path.replace("task/", "").length > 0) {
+    if (path.indexOf("task") >= 0 && path.replace("task/", "").length > 0) {
       this.sampleInteractionId = path.replace("/task/", "");
       logger.info("Current interactionID: ", this.sampleInteractionId);
     }
@@ -70,10 +183,12 @@ export default class MyCustomComponent extends LitElement {
     const buddyAgentPayload = {
       agentProfileId: "AXCLfZhH9S1oTdqE1OFw",
       channelName: "chat",
-      state: "Available"
+      state: "Available",
     };
-     
-    this.buddyAgents = await agentxJsApi.agentContact.buddyAgents({ data: buddyAgentPayload });
+
+    this.buddyAgents = await agentxJsApi.agentContact.buddyAgents({
+      data: buddyAgentPayload,
+    });
     logger.info(this.buddyAgents);
   }
 
@@ -82,16 +197,18 @@ export default class MyCustomComponent extends LitElement {
       agentProfileId: "AXCLfZhH9S1oTdqE1OFw",
       agentSessionId: "5a84d32c-691b-4500-b163-d6cdba2a3163",
       channelType: "chat",
-      type: "inboundqueue"
+      type: "inboundqueue",
     };
-     
-    this.vTeam = await agentxJsApi.agentContact.vteamList({ data: vTeamPayload });
-    logger.info(this.vTeam);
+
+    this.vTeam = await agentxJsApi.agentContact.vteamList({
+      data: vTeamPayload,
+    });
+    logger.info(this.vTeam, this.vTeam!.data.data.vteamList);
   }
 
   async endInteraction() {
     let endedInteraction = await agentxJsApi.agentContact.end({
-      interactionId: this.sampleInteractionId
+      interactionId: this.sampleInteractionId,
     });
     logger.info(endedInteraction);
   }
@@ -103,7 +220,7 @@ export default class MyCustomComponent extends LitElement {
         wrapUpReason: "Example reason here",
         auxCodeId: "0",
         isAutoWrapup: "false",
-        }
+      },
     });
     logger.info(wrappedUpInteraction);
   }
@@ -114,10 +231,10 @@ export default class MyCustomComponent extends LitElement {
       data: {
         agentId: this.agentId,
         destAgentId: this.buddyAgents?.data.agentList[0].agentId,
-        mediaType: "chat"
+        mediaType: "chat",
       },
-      url: "consult"
-    })
+      url: "consult",
+    });
     logger.info(consult);
   }
 
@@ -127,25 +244,25 @@ export default class MyCustomComponent extends LitElement {
       data: {
         agentId: this.agentId,
         destAgentId: this.buddyAgents?.data.agentList[0].agentId,
-        mediaType: "chat"
-      }
-    })
+        mediaType: "chat",
+      },
+    });
     logger.info(consult);
   }
 
   async endConferenceWithAgent() {
     let consult = await agentxJsApi.agentContact.consultEnd({
       interactionId: this.sampleInteractionId,
-      isConsult: false
-    })
+      isConsult: false,
+    });
     logger.info(consult);
   }
 
   async endConsultConferenceWithAgent() {
     let consult = await agentxJsApi.agentContact.consultEnd({
       interactionId: this.sampleInteractionId,
-      isConsult: true
-    })
+      isConsult: true,
+    });
     logger.info(consult);
   }
 
@@ -156,9 +273,9 @@ export default class MyCustomComponent extends LitElement {
         agentId: this.agentId,
         destAgentId: this.buddyAgents?.data.agentList[0].agentId,
         mediaType: "chat",
-        mediaResourceId: "b102ed10-fac2-4f8e-bece-1c2da6ba6dd8"
-      }
-    })
+        mediaResourceId: "b102ed10-fac2-4f8e-bece-1c2da6ba6dd8",
+      },
+    });
     logger.info(consult);
   }
 
@@ -167,9 +284,9 @@ export default class MyCustomComponent extends LitElement {
       interactionId: this.sampleInteractionId,
       data: {
         vteamId: this.vTeam?.data.data.vteamList[0].analyzerId,
-        vteamType: this.vTeam?.data.data.vteamList[0].type
-      }
-    })
+        vteamType: this.vTeam?.data.data.vteamList[0].type,
+      },
+    });
     logger.info(consult);
   }
 
@@ -178,7 +295,7 @@ export default class MyCustomComponent extends LitElement {
       <div class="container">
         <h1>Custom Component Contents</h1>
         <md-tabs>
-          <md-tab slot="tab">Request Data</md-tab>
+          <md-tab slot="tab">agentxJsApi.agentStateInfo</md-tab>
           <md-tab-panel slot="panel">
             <div class="action-container">
               <h2>Monitor data output in console log</h2>
@@ -188,6 +305,7 @@ export default class MyCustomComponent extends LitElement {
               <md-button @button-click=${() => this.getClientLocale()}
                 >Get current Locale</md-button
               >
+
               <md-button @button-click=${() => this.changeState("Idle")}
                 >Change State to Idle</md-button
               >
@@ -225,41 +343,86 @@ export default class MyCustomComponent extends LitElement {
               >
 
               <h3>Consult</h3>
-              <md-label>Fetch buddy agents before initiating a consult</md-label>
+              <md-label
+                >Fetch buddy agents before initiating a consult</md-label
+              >
               <md-button @button-click=${() => this.consultWithAgent()}
-                >Consult with ${this.buddyAgents?.data.agentList[0].agentId}</md-button
+                >Consult with
+                ${this.buddyAgents && this.buddyAgents?.data.agentList.length > 0
+                  ? this.buddyAgents?.data.agentList[0].agentId
+                  : "No Agents available"}</md-button
               >
 
-              <md-label>Make sure to have active conference before ending</md-label>
+              <md-label
+                >Make sure to have active conference before ending</md-label
+              >
               <md-button @button-click=${() => this.endConferenceWithAgent()}
-                >End Consult with ${this.buddyAgents?.data.agentList[0].agentId}</md-button
+                >End Consult with
+                ${this.buddyAgents && this.buddyAgents?.data.agentList.length > 0
+                  ? this.buddyAgents?.data.agentList[0].agentId
+                  : "No Agents available"}</md-button
               >
 
               <h3>Consult Conference</h3>
 
-              <md-button @button-click=${() => this.consultConferenceWithAgent()}
-                >Consult Conference with ${this.buddyAgents?.data.agentList[0].agentId}</md-button
+              <md-button
+                @button-click=${() => this.consultConferenceWithAgent()}
+                >Consult Conference with
+                ${this.buddyAgents && this.buddyAgents?.data.agentList.length > 0
+                  ? this.buddyAgents?.data.agentList[0].agentId
+                  : "No Agents available"}</md-button
               >
-              <md-label>Make sure to have active conference before ending</md-label>
-              <md-button @button-click=${() => this.endConsultConferenceWithAgent()}
-                >End Consult Conference with ${this.buddyAgents?.data.agentList[0].agentId}</md-button
+              <md-label
+                >Make sure to have active conference before ending</md-label
+              >
+              <md-button
+                @button-click=${() => this.endConsultConferenceWithAgent()}
+                >End Consult Conference with
+                ${this.buddyAgents && this.buddyAgents?.data.agentList.length > 0
+                  ? this.buddyAgents?.data.agentList[0].agentId
+                  : "No Agents available"}</md-button
               >
 
               <h3>Consult Transfer</h3>
               <md-button @button-click=${() => this.consultTransferToAgent()}
-                >Consult Transfer to ${this.buddyAgents?.data.agentList[0].agentId}</md-button
+                >Consult Transfer to
+                ${this.buddyAgents && this.buddyAgents?.data.agentList.length > 0
+                  ? this.buddyAgents?.data.agentList[0].agentId
+                  : "No Agents available"}</md-button
               >
 
               <h3>vTeam Transfer</h3>
               <md-label>Fetch vTeam before initiating a transfer</md-label>
               <md-button @button-click=${() => this.vTeamTransfer()}
-                >Consult Transfer to ${this.vTeam?.data.data.vteamList[0].analyzerId}</md-button
+                >Consult Transfer to
+                ${this.vTeam?.data.data.vteamList[0].analyzerId}</md-button
               >
-
             </div>
           </md-tab-panel>
 
-          <md-tab slot="tab">Two</md-tab>
+          <md-tab slot=“tab”>agentxJsApi.shortcutKey</md-tab>
+          <md-tab-panel slot=“panel”>
+            <h2>Monitor data output in console log</h2>
+            <div class=“action-container”>
+              <md-button @button-click=${() => logger.info(agentxJsApi.shortcutKey.DEFAULT_SHORTCUT_KEYS)}
+                  >Get Default Shortcut Keys</md-button
+                >
+
+                <md-button @button-click=${() => logger.info(agentxJsApi.shortcutKey.MODIFIERS)}
+                  >Get Shortcut Keys Modifiers</md-button
+                >
+
+                <md-button @button-click=${() => logger.info(agentxJsApi.shortcutKey.REGISTERED_KEYS)}
+                  >Get all registered Shortcut Keys</md-button
+                >
+
+                <md-button @button-click=${() => logger.info(agentxJsApi.shortcutKey.getRegisteredKeys())}
+                  >Get all registered Shortcut Keys</md-button
+                >
+            </div>
+          </md-tab-panel>
+
+          <md-tab slot="tab">Vertical overflow tab</md-tab>
           <md-tab-panel slot="panel">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
