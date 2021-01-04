81d5c54e1bd71074abeb4cd9e568335ba709b901
Elena Giere <egiere@cisco.com>
2021-01-04 14:58:16 -0500

    covid-by-location widget in Examples/Widgets directory

---
+++ Examples/Widgets/CovidByLocation/src/[sandbox]/sandbox.ts
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) Cisco Systems, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import "@momentum-ui/web-components";
+import { customElement, html, internalProperty, LitElement } from "lit-element";
+import "../index";
+import { agentContactData } from "./sandbox.mock";
+import styles from "./sandbox.scss";
+
+@customElement("wcc-widget-starter-lit")
+export class Sandbox extends LitElement {
+  @internalProperty() darkTheme = false;
+  @internalProperty() containerWidth = "600px";
+  @internalProperty() containerHeight = "610px";
+
+  static get styles() {
+    return styles;
+  }
+
+  themeToggle() {
+    return html`
+      <div class="toggle-container">
+        <md-checkbox
+          type="checkbox"
+          id="theme-switch"
+          class="theme-switch"
+          data-aspect="darkTheme"
+          label="Dark Mode"
+          @checkbox-change=${(e: MouseEvent) => this.toggleSetting(e)}
+          ?checked=${this.darkTheme}
+          >Dark Mode</md-checkbox
+        >
+        <div class="switch-container">
+          <md-label class="switch" text="Responsive">
+            Widget Boundary
+          </md-label>
+          <md-input
+            type="text"
+            id="width-switch"
+            class="theme-switch"
+            data-aspect="responsive-width"
+            @click=${(e: MouseEvent) => this.toggleSetting(e)}
+            @input-change=${(e: MouseEvent) => this.toggleSetting(e)}
+            value=${this.containerWidth}
+          ></md-input>
+          <md-label>x</md-label>
+          <md-input
+            type="text"
+            id="height-switch"
+            class="theme-switch"
+            data-aspect="responsive-height"
+            @click=${(e: MouseEvent) => this.toggleSetting(e)}
+            @input-change=${(e: MouseEvent) => this.toggleSetting(e)}
+            value=${this.containerHeight}
+          ></md-input>
+        </div>
+        <div class="switch-container">
+          <md-label class="switch" text="New Contact">
+            Send Contact
+          </md-label>
+          <md-button @button-click=${() => this.sendContact()}
+            >Send Chat Contact</md-button
+          >
+        </div>
+      </div>
+    `;
+  }
+
+  toggleSetting(e: MouseEvent) {
+    const composedPath = e.composedPath();
+    const target = (composedPath[0] as unknown) as HTMLInputElement;
+    const aspect: string = target.dataset.aspect!;
+    if (aspect === "responsive-width") {
+      this.containerWidth = target.value;
+    } else if (aspect === "responsive-height") {
+      this.containerHeight = target.value;
+    } else if (aspect === "darkTheme") {
+      this.darkTheme = !this.darkTheme;
+    } else return console.error("Invalid data-aspect input");
+  }
+
+  sendContact() {
+    let contactEvent: any /** Service.Aqm.Contact.AgentContact **/ = {
+      type: "",
+      orgId: "",
+      trackingId: "",
+      data: agentContactData,
+    };
+    const event = new CustomEvent("eAgentContact", {
+      detail: contactEvent,
+      composed: true,
+      bubbles: true
+    });
+    document.dispatchEvent(event);
+  }
+
+  render() {
+    return html`
+    <div class="toggle">
+        ${this.themeToggle()}
+      </div>
+      <md-theme lumos ?darkTheme=${this.darkTheme}>
+        <div class="container">
+          <div style=${`width: ${this.containerWidth}; height: ${this.containerHeight};`} class="widget-container">
+          <covid-by-location></covid-by-location>
+          </div>
+          </div>
+        </md-theme>
+        </div>
+      </md-theme>
+    `;
+  }
+}
---
+++ Examples/Widgets/CovidByLocation/src/components/App.ts
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) Cisco Systems, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import {
+  html,
+  LitElement,
+  customElement,
+  internalProperty,
+  PropertyValues,
+  property,
+} from "lit-element";
+import styles from "./App.scss";
+import "./Graph";
+import "./Table";
+import "./Hospitals";
+
+@customElement("my-custom-component")
+export default class MyCustomComponent extends LitElement {
+  @property() selectedCountyState = "Santa Clara County, CA";
+
+  @internalProperty() countyOptions: Array<string> = [];
+  @internalProperty() selectedCounty = "";
+  @internalProperty() selectedStatePostal = "";
+  @internalProperty() selectedCountyFIPS = "";
+  @internalProperty() stateCountyData: Array<Object> = [];
+
+  static get styles() {
+    return styles;
+  }
+
+  fetchUsersLocation = async () => {
+    return fetch("https://ipapi.co/json/")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log("[log] location data", data);
+        return data;
+      });
+  };
+
+  fetchAllCounties = async () => {
+    const key = "1318b408f01c4aa3b5f79dedc6c90848";
+
+    return await fetch(
+      `https://api.covidactnow.org/v2/counties.json?apiKey=${key}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        return data;
+      });
+  };
+
+  generateCollections = async (allUSACounties: Array<{ fips: string, county: string, state: string }>) => {
+    this.stateCountyData = [];
+    const generatedStateCountyData: Object[] = [];
+    this.countyOptions = [];
+    const generatedCountyOptions: string[] = [];
+
+    allUSACounties.forEach(countyData => {
+      generatedCountyOptions.push(`${countyData.county}, ${countyData.state}`);
+
+      if (this.selectedStatePostal && this.selectedStatePostal === countyData.state) {
+        generatedStateCountyData.push(countyData);
+
+        if (this.selectedCounty && this.selectedCounty === countyData.county) {
+          this.selectedCountyFIPS = countyData.fips;
+        }
+      }
+    });
+
+    this.stateCountyData = generatedStateCountyData;
+    this.countyOptions = generatedCountyOptions;
+  };
+
+  handleStateSelection = (event: CustomEvent) => {
+    this.selectedCountyState = event?.detail?.value;
+    this.parseCountyAndState();
+    console.log("[log] handleStateSelection", this.selectedCounty, this.selectedStatePostal);
+  };
+
+  parseCountyAndState = () => {
+    this.selectedCounty = this.selectedCountyState.split(', ')[0];
+    this.selectedStatePostal = this.selectedCountyState.split(', ')[1];
+  }
+
+  async firstUpdated(changeProperties: PropertyValues) {
+    super.firstUpdated(changeProperties);
+
+    if (!this.selectedStatePostal) {
+      await this.fetchUsersLocation().then(userLocationData => {
+        console.log("[log] fetchUsersLocation DATA", userLocationData);
+        // TODO figure out county user Location
+
+        this.selectedStatePostal = userLocationData.region_code;
+      });
+    }
+
+    if (this.selectedCountyState) {
+      this.parseCountyAndState();
+      await this.fetchAllCounties().then(result => {
+        this.generateCollections(result);
+      });
+    }
+  }
+
+  async updated(changeProperties: PropertyValues) {
+    super.updated(changeProperties);
+
+    if (changeProperties.has('selectedCountyState')) {
+      this.parseCountyAndState();
+      await this.fetchAllCounties().then(result => {
+        this.generateCollections(result);
+      });
+    }
+  }
+
+  render() {
+    return html`
+      <div class="container">
+        <div class="main-header">
+          <span class="header-text">COVID Cases by Location</span>
+          <md-button circle hasRemoveStyle
+            ><md-icon slot="icon" name="maximize_16"></md-icon
+          ></md-button>
+        </div>
+        <div class="body">
+          <div class="combobox-row">
+            <md-combobox
+              shape="pill"
+              class="city-combobox"
+              .options=${this.countyOptions}
+              placeholder="Placeholder"
+              .value=${[this.selectedCountyState]}
+              @change-selected="${(e: CustomEvent) =>
+                this.handleStateSelection(e)}"
+            >
+            </md-combobox>
+          </div>
+        </div>
+        <div class="cases-by-location">
+          <my-graph
+            class="graph-widget"
+            selectedCountyFIPS=${this.selectedCountyFIPS}
+          ></my-graph>
+          <my-table class="table-widget" .stateCountyData=${this.stateCountyData}></my-table>
+        </div>
+        <my-hospital-stats class="hospital-stats-widget"></my-hospital-stats>
+      </div>
+    `;
+  }
+}
