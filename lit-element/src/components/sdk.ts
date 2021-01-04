17f07ac2f3f6ad16e1cc2549ddf795b4cd3913e0
Kevin Hyde <kehyde@cisco.com>
2021-01-04 11:34:54 -0800

    reclaim original litelement folder

---
+++ lit-element/src/components/sdk.ts
@@ -0,0 +1,6 @@
+import { agentxJsApi } from "@agentx/agentx-js-api";
+
+/**
+ * Replace the namespace here according to your widget/component name
+ */
+export const logger = agentxJsApi.logger.createLogger("my-custom-widget");
