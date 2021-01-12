f1743553dc44b5a1042ddc8c49b8870148b615e2
Kevin Hyde <kehyde@cisco.com>
2021-01-12 15:10:54 -0800

    Remove unneeded files

--- Examples/Widgets/RSS/lit-element/src/components/sdk.ts
+++
@@ -1,6 +0,0 @@
-import { agentxJsApi } from "@agentx/agentx-js-api";
-
-/**
- * Replace the namespace here according to your widget/component name
- */
-export const logger = agentxJsApi.logger.createLogger("my-custom-widget");
