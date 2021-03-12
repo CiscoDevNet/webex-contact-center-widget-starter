55254b7a42632dc345e8fc646386a7421867b3e9
Lena Vaganova (helenv) <helenv@cisco.com>
2021-03-12 10:59:48 +0200

    build(widget): create basic notes widget configuration

---
+++ Examples/Widgets/Note/webpack.config.dev.server.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Cisco Systems, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import merge from "webpack-merge";
+import { commonDev, pBuild } from "./webpack.config";
+
+export default merge(commonDev, {
+  devServer: {
+    contentBase: pBuild,
+    historyApiFallback: true,
+    open: true,
+    hot: true,
+    port: 8888
+  }
+});
