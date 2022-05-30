9d2b17b01a7bed7eea6de45a2edeab8f81dde93f
Parag Somani <parsoman@cisco.com>
2022-05-31 00:03:39 +0530

    Reverting this change

--- Examples/Widgets/Notes/src/index.ts
+++ Examples/Widgets/Notes/src/index.ts
@@ -3,7 +3,7 @@ import "@momentum-ui/web-components";
 import app from "./assets/localisation/app.json";
 import { customElement, html, LitElement, property } from "lit-element";
 import style from "./assets/styles/NotesHeaderWidget.scss";
-// import "./NotesWidget";
+import "./NotesWidget";
 
 export namespace NotesHeaderWidget {
   /**
@@ -30,47 +30,9 @@ export namespace NotesHeaderWidget {
             ></md-button>
           </md-tooltip>
           <div class="notes-widget-container">
-            <!-- <notes-widget .agentId=${this.agentId}></notes-widget> -->
+            <notes-widget .agentId=${this.agentId}></notes-widget>
           </div>
         </md-menu-overlay>
-
-        <md-button class="notes-btn" variant="secondary" size="40" circle
-              ><md-icon name="icon-response_16"></md-icon
-            ></md-button>
-
-            <md-button class="notes-btn" variant="secondary" size="40" circle
-              ><md-icon name="icon-lock_12"></md-icon
-            ></md-button>
-            <md-button class="notes-btn" variant="secondary" size="40" circle
-              ><md-icon name="icon-add_16"></md-icon
-            ></md-button>
-            <md-button class="notes-btn" variant="secondary" size="40" circle
-              ><md-icon name="icon-filter_16"></md-icon
-            ></md-button>
-            <md-icon name="icon-pin_16"></md-icon
-            >
-            <md-icon name="icon-chat_14"></md-icon
-            >
-            <md-icon name="icon-cancel_16"></md-icon
-            >
-            <md-icon name="icon-apps_16"></md-icon
-            >
-            <md-icon name="icon-error_16"></md-icon
-            >
-            <md-icon name="icon-handset_16"></md-icon
-            >
-            <md-icon name="icon-contact-card_16"></md-icon
-            >
-            <md-icon name="icon-dialpad_16"></md-icon
-            >
-            <md-icon name="icon-refresh_16"></md-icon
-            >
-            <md-icon name="icon-send_16"></md-icon
-            >
-            <md-icon name="icon-more_16"></md-icon
-            >
-            <md-icon name="icon-attachment_16"></md-icon
-            >
       `;
     }
   }
