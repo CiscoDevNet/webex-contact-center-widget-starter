864e518a41bfea5967c4e567b3a79deccf585d16
molker <matthew.olker@gmail.com>
2023-04-20 09:53:06 -0400

    removing extra readme and comments

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,21 +1,3 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
 import { Injector, NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
